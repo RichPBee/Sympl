87082fbdfc88a9d32d838905e70e3b89bc032958
ArrBee <81438594+RichPBee@users.noreply.github.com>
2023-11-14 20:45:10 +0000

    Module is now exportable and usable in other files:
    Fixed circular dependencies, scrapped namespace system in favour of simple exports
    also have removed webpack, as it turns out it's not needed here. From here it should be possible
    to build webapps and expand on this library from there.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,1 @@
 export { SymplApp } from './App';
-export { BaseView } from './Views/View';
-export { ViewController } from './Renderer/ViewController';
-export { ElementBuilder } from './Renderer/ElementBuilder';
-export { Renderer } from './Renderer/Renderer';
-export { BaseComponent } from './Components/BaseComponent';
